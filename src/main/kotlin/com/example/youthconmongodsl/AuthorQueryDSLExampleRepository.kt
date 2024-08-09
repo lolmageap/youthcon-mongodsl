f7aa56eed541a9c3ad4634833cde9a7d76c6ebd7
lolmageap <ekxk1234@naver.com>
2024-08-09 23:41:32 +0900

    fix to querydsl example

--- src/main/kotlin/com/example/youthconmongodsl/AuthorQueryDSLExampleRepository.kt
+++ src/main/kotlin/com/example/youthconmongodsl/AuthorQueryDSLExampleRepository.kt
@@ -2,34 +2,30 @@ package com.example.youthconmongodsl
 
 import com.example.youthconmongodsl.collection.Author
 import com.example.youthconmongodsl.collection.QAuthor
+import com.querydsl.core.types.dsl.BooleanExpression
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 
-interface AuthorRepositoryQueryDSL : MongoRepository<Author, String>, QuerydslPredicateExecutor<Author>
-
-interface AuthorRepositoryPredicate {
+interface AuthorPredicateBuilder {
     fun findAuthors(
         names: List<String>,
         minAge: Int?,
         maxAge: Int?,
         nickname: String?,
         pageable: Pageable,
-    ): Page<Author>
+    ): BooleanExpression
 }
 
-class AuthorRepositoryQueryDSLImpl(
-    private val authorRepository: AuthorRepositoryQueryDSL,
-) : AuthorRepositoryPredicate {
-
+class AuthorPredicateBuilderImpl : AuthorPredicateBuilder {
     override fun findAuthors(
         names: List<String>,
         minAge: Int?,
         maxAge: Int?,
         nickname: String?,
         pageable: Pageable,
-    ): Page<Author> {
+    ): BooleanExpression {
         val author = QAuthor.author
         var predicate = author.name.`in`(names)
         if (minAge != null && maxAge != null) {
@@ -42,6 +38,24 @@ class AuthorRepositoryQueryDSLImpl(
             maxAge?.let { predicate = predicate.and(author.age.lt(it)) }
         }
         nickname?.let { predicate = predicate.and(author.nickname.contains(it)) }
+        return predicate
+    }
+}
+
+interface AuthorRepositoryQueryDSL : MongoRepository<Author, String>, QuerydslPredicateExecutor<Author>
+
+class AuthorService(
+    private val authorRepository: AuthorRepositoryQueryDSL,
+    private val authorPredicateBuilder: AuthorPredicateBuilder,
+) {
+    fun findAuthors(
+        names: List<String>,
+        minAge: Int?,
+        maxAge: Int?,
+        nickname: String?,
+        pageable: Pageable,
+    ): Page<Author> {
+        val predicate = authorPredicateBuilder.findAuthors(names, minAge, maxAge, nickname, pageable)
         return authorRepository.findAll(predicate, pageable)
     }
 }
