f0c10bd02fc741ed6f5ed27e83fbb36e38433aef
lolmageap <ekxk1234@naver.com>
2024-08-18 21:49:39 +0900

    youthcon'24 시나리오 재작성 및 Atlas 연결

--- src/test/kotlin/com/example/youthconmongodsl/Scenario1.kt
+++ src/test/kotlin/com/example/youthconmongodsl/Scenario1.kt
@@ -13,21 +13,20 @@ import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 
 /**
- * 1. MongoDB의 쿼리를 작성할 때 앞서 말한 대로 JSON 형태로 작성합니다.
+ * 1. MongoDB의 Query를 작성할 때 앞서 말한 대로 JSON 형태로 작성합니다.
  *    쌍따옴표를 3개 사용하면 문자열을 여러 줄에 걸쳐 작성할 수 있습니다.
  *    trimIndent()는 문자열의 앞, 뒷부분에 있는 공백을 제거합니다.
  *
- * 2. 위와 같은 몽고 쿼리를 Kotlin에서 사용하려면 MongoTemplate과 같은 별도의 Dependency가 필요합니다.
+ * 2. 위와 같은 MongoDB의 Query를 Spring에서 사용하려면 MongoTemplate과 같은 별도의 Dependency가 필요합니다.
  *    JSON 형태의 문자열 쿼리를 항상 작성하는건 비효율적이기 때문에 이번 실습은 Criteria를 사용해서 쿼리를 작성하려고 합니다.
- *    Criteria는 Spring Data MongoDB에서 제공하는 쿼리 작성을 위한 클래스입니다.
+ *
  */
-
 @SpringBootTest
 class Scenario1(
     @Autowired private val mongoTemplate: MongoTemplate,
 ) {
     @Test
-    fun `MongoTemplate에서 Native Query를 사용한 조회`() {
+    fun `MongoTemplate에서 Native Query를 사용한 조회 as is`() {
         val json = """
             {
                 "name": "youthcon",
@@ -37,11 +36,13 @@ class Scenario1(
 
         val jsonObject = JSONObject(json).toString()
         val nativeQuery = BasicQuery(Document.parse(jsonObject))
-        mongoTemplate.findOne(nativeQuery, Author::class.java)
+
+        val author = mongoTemplate.findOne(nativeQuery, Author::class.java)
+        Assertions.assertThat(author).isNotNull
     }
 
     @Test
-    fun `MongoTemplate에서 Criteria를 사용한 조회`() {
+    fun `MongoTemplate에서 Criteria를 사용한 조회 to be`() {
         val criteria = Criteria().andOperator(
             Criteria.where("name").`is`("youthcon").and("age").`is`(30)
         )
--- src/test/kotlin/com/example/youthconmongodsl/Scenario3.kt
+++ src/test/kotlin/com/example/youthconmongodsl/Scenario3.kt
@@ -1,6 +1,7 @@
 package com.example.youthconmongodsl
 
 import com.example.youthconmongodsl.collection.Author
+import com.example.youthconmongodsl.collection.Status
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,90 +10,112 @@ import org.springframework.data.domain.Sort
 import org.springframework.data.domain.Sort.Direction
 import org.springframework.data.domain.Sort.Direction.*
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.AggregationResults
+import org.springframework.data.mongodb.core.aggregation.GroupOperation
+import org.springframework.data.mongodb.core.aggregation.MatchOperation
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.and
 import org.springframework.data.mongodb.core.query.where
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
 /**
- * 1. 첫번째 시나리오에서 andOperator를 만들었던 것 처럼 orOperator를 만들어서 Criteria를 생성해도 될것 습니다.
+ *    이번에는 통계 쿼리를 작성해보겠습니다.
+ *    일반적으로 RDBMS에서는 GROUP BY를 사용하여 통계 쿼리를 작성합니다.
+ *    MongoDB에서는 Aggregation Framework를 사용하여 통계 쿼리를 작성합니다.
+ *    현재 작성된 코드에 대해서 간단하게 설명하겠습니다.
  *
- * 2. where(key).exists(false) <- 이러한 부분을 코틀린의 infix 함수를 사용하여 변경할 수 있을것 같습니다.
- *    infix 함수는 중위 함수라고도 불리며, 함수를 호출할때 .을 생략할 수 있습니다.
- *    하지만 infix 함수를 사용한다고 무조건 가독성이 좋아지는것은 아닙니다.
- *    복잡한 연산을 할 때 gte 함수와 and 함수, lte 함수 모두 infix 함수로 만들면 가독성이 떨어질 수 있습니다.
- *    한번 직접 코드로 작성해보고 가독성을 확인해보시죠!
+ *    1. MatchOperation을 사용하여 나이가 20대인 키가 160cm 이상인 작가를 필터링합니다.
+ *    2. GroupOperation을 사용하여 상태별로 그루핑한 뒤 가진 돈의 합을 구합니다.
+ *    3. MatchOperation 과 GroupOperation을 하나의 Aggregation으로 묶어서 실행합니다.
+ *    4. 결과를 Map으로 받아와서 상태별로 가진 돈의 합을 구합니다.
  *
- * 3. 정렬(sort)을 할때도 with(Sort.by(Sort.Order.asc("age"))) 이 부분도 더 깔끔하게 변경할 수 있을것 같습니다.
- *    우리는 조금 더 명시적으로 orderBy 라는 키워드를 사용해서 함수를 한번 만들어보겠습니다.
+ *    MongoDB를 모르는 사람은 이 코드를 이해하기 어려울 수 있습니다.
+ *    그래서 우리는 일반적인 RDBMS의 Group By 쿼리처럼 만들어보려고 합니다.
+ *    이번에는 Criteria를 확장함수를 사용하여 Group By 쿼리를 작성해보겠습니다.
+ *
+ *    어떤 필드를 그루핑 할 건지 알려주는 groupBy 함수와 그루핑한 뒤 합계를 구하는 sum 함수를 만들어보겠습니다.
+ *    그리고 이러한 조건들을 만족 시킬 수 있는 Group이라는 클래스도 별도로 만들어주겠습니다.
+ *    이렇게 하면 MongoDB의 Aggregation Framework를 사용하지 않고도 Group By 쿼리를 작성할 수 있습니다.
  */
-
 @SpringBootTest
 class Scenario3(
     @Autowired private val mongoTemplate: MongoTemplate,
 ) {
     @Test
-    fun `별명이 존재하지 않는 작가 또는 나이가 50 이상이고 키가 160cm 이하인 작가를 나이가 많은 순으로 조회한다 as is`() {
-        val criteria = Criteria().orOperator(
-            where("nickname").exists(false),
-            where("age").gte(50).and("height").lte(160),
+    fun `나이가 20대이고 키가 160cm 이상인 작가를 상태별로 그루핑한 뒤 가진 돈의 합을 구한다 as is`() {
+        val criteria = Criteria().andOperator(
+            Criteria.where("age").gte(20).lte(29),
+            Criteria.where("height").gte(160),
         )
 
-        val query = Query.query(criteria).with(Sort.by(Sort.Order.desc("age")))
-        val authors = mongoTemplate.find(query, Author::class.java)
+        val matchOperation = MatchOperation(criteria)
+        val groupOperation = Aggregation.group("status").sum("money").`as`("totalMoney")
+        val aggregation = Aggregation.newAggregation(matchOperation, groupOperation)
 
-        val filteredAuthor =
-            authors.filter { it.nickname == null }
-                .filter { it.age >= 50 }
-                .filter { it.height!! <= 160 }
+        val aggregationResults = mongoTemplate.aggregate(aggregation, Author::class.java, Map::class.java)
 
-        Assertions.assertThat(authors).isEqualTo(filteredAuthor)
+
+        // 검증
+        val statusToAverageMoney = aggregationResults.mappedResults.associate {
+            val status = Status.valueOf(it["_id"].toString())
+            val total = it["totalMoney"] as Long
+            status to total
+        }
+
+        Assertions.assertThat(statusToAverageMoney[Status.ACTIVE]).isEqualTo(7264569615)
+        Assertions.assertThat(statusToAverageMoney[Status.REST]).isEqualTo(7327825153)
+        Assertions.assertThat(statusToAverageMoney[Status.RETIREMENT]).isEqualTo(7566660567)
     }
 
     @Test
-    fun `별명이 존재하지 않는 작가 또는 나이가 50 이상이고 키가 160cm 이하인 작가를 나이가 많은 순으로 조회한다 to be`() {
-        val criteria = orOperator(
-            where(Author::nickname) exists false,
-            where(Author::age) gte 50 and Author::height lte 160,
-        ).toQuery()
-            .orderBy(Author::age, DESC)
+    fun `나이가 20대이고 키가 160cm 이상인 작가를 상태별로 그루핑한 뒤 가진 돈의 합을 구한다 to be`() {
+        val aggregation = andOperator(
+            where(Author::age) gte 20 lte 29,
+            where(Author::height) gte 160,
+        ).groupBy(Author::status)
+            .sum(Author::money, "totalMoney")
 
-        val authors = mongoTemplate.find(criteria, Author::class)
+        val aggregationResults = mongoTemplate.aggregate(aggregation, Author::class.java, Map::class.java)
 
-        val filteredAuthor =
-            authors.filter { it.nickname == null }
-                .filter { it.age >= 50 }
-                .filter { it.height!! <= 160 }
 
-        Assertions.assertThat(authors).isEqualTo(filteredAuthor)
+        // 검증
+        val statusToAverageMoney = aggregationResults.mappedResults.associate {
+            val status = Status.valueOf(it["_id"].toString())
+            val total = it["totalMoney"] as Long
+            status to total
+        }
+
+        Assertions.assertThat(statusToAverageMoney[Status.ACTIVE]).isEqualTo(7264569615)
+        Assertions.assertThat(statusToAverageMoney[Status.REST]).isEqualTo(7327825153)
+        Assertions.assertThat(statusToAverageMoney[Status.RETIREMENT]).isEqualTo(7566660567)
     }
 }
 
-fun orOperator(
-    vararg criteria: Criteria,
-) =
-    Criteria().orOperator(*criteria)
+fun Criteria.groupBy(
+    property: KProperty<*>,
+) = Group(this, property)
 
-infix fun Criteria.exists(
-    value: Boolean,
-) =
-    this.exists(value)
-
-fun Query.orderBy(
-    key: KProperty<*>,
-    direction: Direction,
-) =
-    this.with(
-        Sort.by(
-            Sort.Order(direction, key.name)
-        )
-    )
-
-infix fun Criteria.gte(
-    value: Any,
-) = this.gte(value)
+fun GroupOperation.sum(
+    property: KProperty<*>,
+) = this.sum(property.name)
 
 infix fun Criteria.lte(
     value: Any,
 ) = this.lte(value)
+
+class Group(
+    private val criteria: Criteria,
+    private val property: KProperty<*>,
+) {
+    fun sum(
+        sumProperty: KProperty<*>,
+        alias: String,
+    ) =
+        Aggregation.newAggregation(
+            MatchOperation(criteria),
+            Aggregation.group(property.name).sum(sumProperty.name).`as`(alias)
+        )
+}
