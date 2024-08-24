30e23f616c980f8f2bea4b2bdd1c1e4e3105509f
lolmageap <ekxk1234@naver.com>
2024-08-24 22:20:45 +0900

    시나리오 최종 수정

--- src/test/kotlin/com/example/youthconmongodsl/Scenario1.kt
+++ src/test/kotlin/com/example/youthconmongodsl/Scenario1.kt
@@ -4,18 +4,22 @@ import com.example.youthconmongodsl.collection.Author
 import org.assertj.core.api.Assertions.assertThat
 import org.bson.Document
 import org.json.JSONObject
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.BasicQuery
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 
 @SpringBootTest
 class Scenario1(
     @Autowired private val mongoTemplate: MongoTemplate,
 ) {
     @Test
-    fun `MongoTemplate에서 Native Query를 사용한 조회`() {
+    @DisplayName("MongoTemplate에서 Native Query를 사용한 조회")
+    fun nativeQuery() {
         val json = """
             {
                 "name": "youthcon",
@@ -26,6 +30,19 @@ class Scenario1(
         val jsonObject = JSONObject(json).toString()
         val query = BasicQuery(Document.parse(jsonObject))
 
+
+        // 검증
+        val author = mongoTemplate.findOne(query, Author::class.java)
+        assertThat(author).isNotNull
+    }
+
+    @Test
+    @DisplayName("MongoTemplate에서 Criteria API를 사용한 조회")
+    fun criteriaApi() {
+        val criteria = Criteria().andOperator(
+            Criteria.where("name").`is`("youthcon").and("age").`is`(30)
+        )
+        val query = Query.query(criteria)
 
         // 검증
         val author = mongoTemplate.findOne(query, Author::class.java)
--- src/test/kotlin/com/example/youthconmongodsl/Scenario3.kt
+++ src/test/kotlin/com/example/youthconmongodsl/Scenario3.kt
@@ -2,28 +2,34 @@ package com.example.youthconmongodsl
 
 import com.example.youthconmongodsl.collection.Author
 import com.example.youthconmongodsl.collection.Status
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mapping.toDotPath
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.GroupOperation
 import org.springframework.data.mongodb.core.aggregation.MatchOperation
 import org.springframework.data.mongodb.core.query.Criteria
+import kotlin.reflect.KProperty
 
 @SpringBootTest
+@Suppress("SuspiciousCallableReferenceInLambda")
 class Scenario3(
     @Autowired private val mongoTemplate: MongoTemplate,
 ) {
     @Test
-    fun `나이가 20대이고 책을 3권 낸 작가를 상태별로 그루핑한 뒤 가진 돈의 합을 구한다`() {
+    @DisplayName("나이가 20대이고 책을 3권 낸 작가를 상태별로 그루핑한 뒤 가진 돈의 합을 구한다 as is")
+    fun asIs() {
         val criteria = Criteria().andOperator(
             Criteria.where("age").gte(20).lte(29),
             Criteria.where("books").size(3),
         )
 
         val matchOperation = MatchOperation(criteria)
-        val groupOperation = Aggregation.group("status").sum("money").`as`("totalMoney")
+        val groupOperation = Aggregation.group("status").sum("money").`as`(TOTAL_MONEY)
         val aggregation = Aggregation.newAggregation(matchOperation, groupOperation)
 
         val aggregationResults = mongoTemplate.aggregate(aggregation, Author::class.java, Map::class.java)
@@ -31,13 +37,76 @@ class Scenario3(
 
         // 검증
         val statusToAverageMoney = aggregationResults.mappedResults.associate {
-            val status = Status.valueOf(it["_id"].toString())
-            val total = it["totalMoney"] as Long
+            val status = Status.valueOf(it[ID].toString())
+            val total = it[TOTAL_MONEY] as Long
             status to total
         }
 
-        Assertions.assertThat(statusToAverageMoney[Status.ACTIVE]).isEqualTo(2161328487)
-        Assertions.assertThat(statusToAverageMoney[Status.REST]).isEqualTo(2110033461)
-        Assertions.assertThat(statusToAverageMoney[Status.RETIREMENT]).isEqualTo(2486962006)
+        assertThat(statusToAverageMoney[Status.ACTIVE]).isEqualTo(2161328487)
+        assertThat(statusToAverageMoney[Status.REST]).isEqualTo(2110033461)
+        assertThat(statusToAverageMoney[Status.RETIREMENT]).isEqualTo(2486962006)
+    }
+
+//    @Test
+//    @DisplayName("나이가 20대이고 책을 3권 낸 작가를 상태별로 그루핑한 뒤 가진 돈의 합을 구한다 to be")
+//    fun toBe() {
+//        val aggregation =
+//            where {
+//                Author::age between 20..29
+//                Author::books size 3
+//            } groupBy {
+//                Author::status
+//            } sum {
+//                Author::money alias TOTAL_MONEY
+//            }
+//
+//        val aggregationResults = mongoTemplate.aggregate(aggregation, Author::class.java, Map::class.java)
+//
+//
+//        // 검증
+//        val statusToAverageMoney = aggregationResults.mappedResults.associate {
+//            val status = Status.valueOf(it[ID].toString())
+//            val total = it[TOTAL_MONEY] as Long
+//            status to total
+//        }
+//
+//        assertThat(statusToAverageMoney[Status.ACTIVE]).isEqualTo(2161328487)
+//        assertThat(statusToAverageMoney[Status.REST]).isEqualTo(2110033461)
+//        assertThat(statusToAverageMoney[Status.RETIREMENT]).isEqualTo(2486962006)
+//    }
+
+    companion object {
+        private const val ID = "_id"
+        private const val TOTAL_MONEY = "totalMoney"
     }
 }
+
+//class Group(
+//    private val criteria: Criteria,
+//    private val groupProperty: KProperty<*>,
+//) {
+//    class Sum(
+//        private val groupOperation: GroupOperation,
+//    ) {
+//        infix fun KProperty<*>.alias(
+//            value: String,
+//        ): GroupOperation {
+//            return groupOperation.sum(this.toDotPath()).`as`(value)
+//        }
+//    }
+//
+//    infix fun sum(
+//        block: Sum.() -> GroupOperation,
+//    ): Aggregation {
+//        val matchOperation = MatchOperation(criteria)
+//        val groupOperation = Sum(Aggregation.group(groupProperty.toDotPath())).block()
+//        return Aggregation.newAggregation(matchOperation, groupOperation)
+//    }
+//}
+//
+//infix fun Criteria.groupBy(
+//    block: () -> KProperty<*>,
+//) = Group(
+//    this,
+//    block.invoke(),
+//)
