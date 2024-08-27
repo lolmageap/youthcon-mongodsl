77227770df8892e77d4b78d8c5f726598f30f8ed
lolmageap <ekxk1234@naver.com>
2024-08-27 17:19:34 +0900

    youthcon'24 실습 완성본

--- src/test/kotlin/com/example/youthconmongodsl/Scenario2.kt
+++ src/test/kotlin/com/example/youthconmongodsl/Scenario2.kt
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.domain.Sort
+import org.springframework.data.domain.Sort.Direction
 import org.springframework.data.domain.Sort.Direction.DESC
+import org.springframework.data.mapping.toDotPath
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import kotlin.reflect.KProperty
 
 @SpringBootTest
 class Scenario2(
@@ -19,12 +22,12 @@ class Scenario2(
     @Test
     @DisplayName("대소문자 구분 없이 별명에 hy가 포함되고 나이가 20대인 작가를 나이가 많은 순으로 조회한다")
     fun asIs() {
-        val criteria = Criteria().andOperator(
-            Criteria.where("nickname").regex("hy", "i"),
-            Criteria.where("age").gte(20).lte(29),
-        )
-
-        val query = Query(criteria).with(Sort.by(Sort.Order(DESC, "age")))
+        val query = where {
+            Author::nickname containsIgnoreCase "hy"
+            Author::age between 20..29
+        } order {
+            Author::age by DESC
+        }
 
         // 조회
         val authors = mongoTemplate.find(query, Author::class.java)
@@ -37,4 +40,49 @@ class Scenario2(
         assertThat(authors.size).isEqualTo(ageFilteredAuthors.size)
         assertThat(authors.first().age).isEqualTo(29)
     }
+}
+
+class CriteriaBuilder {
+    val criteriaList = mutableListOf<Criteria>()
+
+    infix fun KProperty<*>.containsIgnoreCase(
+        value: String,
+    ): Criteria {
+        val criteria = Criteria(this.toDotPath()).regex(value, "i")
+        criteriaList.add(criteria)
+        return criteria
+    }
+
+    infix fun KProperty<*>.between(
+        value: IntRange,
+    ): Criteria {
+        val criteria = Criteria(this.toDotPath()).gte(value.first).lte(value.last)
+        criteriaList.add(criteria)
+        return criteria
+    }
+}
+
+fun where(
+    block: CriteriaBuilder.() -> Unit,
+): Criteria {
+    val criteriaBuilder = CriteriaBuilder()
+    criteriaBuilder.block()
+    return Criteria().andOperator(criteriaBuilder.criteriaList)
+}
+
+class Order(
+    private val criteria: Criteria,
+) {
+    infix fun KProperty<*>.by(
+        direction: Direction,
+    ): Query {
+        return Query(criteria).with(Sort.by(Sort.Order(direction, this.toDotPath())))
+    }
+}
+
+infix fun Criteria.order(
+    block: Order.() -> Query,
+): Query {
+    val order = Order(this)
+    return order.block()
 }
--- src/test/kotlin/com/example/youthconmongodsl/Scenario3.kt
+++ src/test/kotlin/com/example/youthconmongodsl/Scenario3.kt
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mapping.toDotPath
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.GroupOperation
 import org.springframework.data.mongodb.core.aggregation.MatchOperation
 import org.springframework.data.mongodb.core.query.Criteria
+import kotlin.reflect.KProperty
 
 @SpringBootTest
 @Suppress("SuspiciousCallableReferenceInLambda")
@@ -20,14 +23,15 @@ class Scenario3(
     @Test
     @DisplayName("대소문자 구분 없이 별명에 hy가 포함되고 나이가 20대인 작가를 상태별로 그루핑한 뒤 가진 돈의 합을 구한다")
     fun asIs() {
-        val criteria = Criteria().andOperator(
-            Criteria.where("nickname").regex("hy", "i"),
-            Criteria.where("age").gte(20).lte(29),
-        )
+        val aggregation = where {
+            Author::nickname containsIgnoreCase "hy"
+            Author::age between 20..29
+        } groupBy {
+            Author::status
+        } sum {
+            Author::money alias TOTAL_MONEY
+        }
 
-        val matchOperation = MatchOperation(criteria)
-        val groupOperation = Aggregation.group("status").sum("money").`as`(TOTAL_MONEY)
-        val aggregation = Aggregation.newAggregation(matchOperation, groupOperation)
 
 
         // 조회
@@ -49,4 +53,33 @@ class Scenario3(
         private const val ID = "_id"
         private const val TOTAL_MONEY = "totalMoney"
     }
+}
+
+class Group(
+    private val criteria: Criteria,
+    private val groupProperty: KProperty<*>,
+) {
+    class Sum(
+        private val groupOperation: GroupOperation,
+    ) {
+        infix fun KProperty<*>.alias(
+            value: String,
+        ): GroupOperation {
+            return groupOperation.sum(this.toDotPath()).`as`(value)
+        }
+    }
+
+    infix fun sum(
+        block: Sum.() -> GroupOperation,
+    ): Aggregation {
+        val matchOperation = MatchOperation(criteria)
+        val groupOperation = Sum(Aggregation.group(groupProperty.toDotPath())).block()
+        return Aggregation.newAggregation(matchOperation, groupOperation)
+    }
+}
+
+infix fun Criteria.groupBy(
+    block: () -> KProperty<*>,
+): Group {
+    return Group(this, block.invoke())
 }
